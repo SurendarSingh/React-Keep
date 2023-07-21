a099f93843989c10b60442e75808bfd33ded9f01
Surendar Singh <surendarsingh2002@gmail.com>
2023-07-22 02:20:12 +0530

    Add and Delete Note functionality added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
-import Form from "./Form";
-
-const isRegistered = true;
-const isLoggedIn = false;
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            {isLoggedIn ? notes.map(note => (
-                <Note
-                    key={note.key}
-                    title={note.title}
-                    content={note.content}
-                />
-            )) : <Form isRegistered={isRegistered}/>}
-            <Footer />
-        </div>
-    );
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note) {
+    setNotes(prevNotes => {
+      return [...prevNotes, note];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, idx) => {
+        return idx !== id;
+      });
+    });
+  }
+
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, idx) => (
+        <Note
+          key={idx}
+          id={idx}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
